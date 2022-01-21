3e9756fbc010fef6af42abb170773ed936b6ffea
VladPAIN <31589193+VladPAIN@users.noreply.github.com>
2022-01-21 22:09:58 +0300

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,6 +5,22 @@
  require("@nomiclabs/hardhat-etherscan");
  require('solidity-coverage');
  require('dotenv').config();
+ require("@nomiclabs/hardhat-web3");
+
+ task("accounts", "Prints accounts", async (_, { web3 }) => {
+  
+  console.log(await web3.eth.getAccounts());
+  
+});
+
+task("balance", "Prints an account's balance")
+  .addParam("account", "The account's address")
+  .setAction(async (taskArgs) => {
+    const account = web3.utils.toChecksumAddress(taskArgs.account);
+    const balance = await web3.eth.getBalance(account);
+
+    console.log(web3.utils.fromWei(balance, "ether"), "ETH");
+});
 
 module.exports = {
   solidity: "0.8.10",
